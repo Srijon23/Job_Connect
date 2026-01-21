ae6c4b9c5bdc5b54f440ff41e878ae7ea0acb106
Srijon23 <139785565+Srijon23@users.noreply.github.com>
2026-01-22 04:35:58 +0600

    Changed

--- src/pages/jobListing.jsx
+++ src/pages/jobListing.jsx
@@ -99,7 +99,7 @@ const JobListing = () => {
           </SelectTrigger>
           <SelectContent>
             <SelectGroup>
-              {State.getStatesOfCountry("IN").map(({ name }) => {
+              {State.getStatesOfCountry("BD").map(({ name }) => {
                 return (
                   <SelectItem key={name} value={name}>
                     {name}
